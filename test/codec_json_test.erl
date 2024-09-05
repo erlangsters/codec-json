1b5030ba8e562c49f096628344e71eda5f01a453
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-09-06 03:04:04 +0800

    Add source file headers (copyright/author)

--- test/codec_json_test.erl
+++ test/codec_json_test.erl
@@ -1,3 +1,12 @@
+%%
+%% Copyright (c) 2025, Byteplug LLC.
+%%
+%% This source file is part of a project made by the Erlangsters community and
+%% is released under the MIT license. Please refer to the LICENSE.txt file that
+%% can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, April 2025
+%%
 -module(codec_json_test).
 -include_lib("eunit/include/eunit.hrl").
 
